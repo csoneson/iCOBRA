b8d56ac8e2ddb30c7e3acf454562ad5987144939
Charlotte Soneson <charlottesoneson@gmail.com>
2017-05-09 16:26:29 +0200

    Fixed small bug in FSR calculations. Correct coloring of points if E[FDR]=alpha

--- tests/testthat/test_constructors.R
+++ tests/testthat/test_constructors.R
@@ -8,7 +8,7 @@ test_that("COBRAData constructors generate COBRAData objects", {
   expect_is(COBRAData_from_text(
     truth_file = system.file("extdata", "cobradata_example_truth.txt",
                              package = "iCOBRA"),
-    result_files = system.file("extdata", "cobradata_example_results.txt",
+    result_files = system.file("extdata", "cobradata_example_results_4.txt",
                                package = "iCOBRA"),
     feature_id = "feature"), "COBRAData")
 
@@ -34,14 +34,14 @@ test_that("COBRAData constructors generate COBRAData objects", {
   padj <- p.adjust(pval, method = "BH")
   score <- 1:5
   ib <- COBRAData(pval = data.frame(m1 = pval, row.names = paste0("F", 1:5)),
-                 padj = data.frame(m1 = padj, row.names = paste0("F", 1:5)),
-                 score = data.frame(m1 = score, row.names = paste0("F", 1:5)),
-                 truth = data.frame(status = c(0, 1, 0, 1, 0),
-                                    row.names = paste0("F", 1:5)))
+                  padj = data.frame(m1 = padj, row.names = paste0("F", 1:5)),
+                  score = data.frame(m1 = score, row.names = paste0("F", 1:5)),
+                  truth = data.frame(status = c(0, 1, 0, 1, 0),
+                                     row.names = paste0("F", 1:5)))
   ib2 <- COBRAData(pval = data.frame(m2 = pval, row.names = paste0("F", 1:5)),
-                  padj = data.frame(m2 = padj, row.names = paste0("F", 1:5)),
-                  score = data.frame(m2 = score, row.names = paste0("F", 1:5)),
-                  object_to_extend = ib)
+                   padj = data.frame(m2 = padj, row.names = paste0("F", 1:5)),
+                   score = data.frame(m2 = score, row.names = paste0("F", 1:5)),
+                   object_to_extend = ib)
   expect_equal(pval(ib2)$m1, pval(ib2)$m2)
   expect_equal(padj(ib2)$m1, padj(ib2)$m2)
   expect_equal(score(ib2)$m1, score(ib2)$m2)
@@ -52,16 +52,19 @@ test_that("extending empty COBRAData object works as expected", {
                                      row.names = paste0("F", 1:5)),
                    padj = data.frame(mA = c(0.1, 0.2, 0.3, 0.4, 0.5),
                                      row.names = paste0("F", 1:5)),
+                   sval = data.frame(mA = c(0.1, 0.2, 0.3, 0.4, 0.5),
+                                     row.names = paste0("F", 1:5)),
                    score = data.frame(mA = c(0.1, 0.2, 0.3, 0.4, 0.5),
                                       row.names = paste0("F", 1:5)),
                    truth = data.frame(status = c(1, 0, 1, 0, 1),
                                       row.names = paste0("F", 1:5)))
   ibB <- COBRAData()
-  ibC <- COBRAData(pval = pval(ibA), padj = padj(ibA),
+  ibC <- COBRAData(pval = pval(ibA), padj = padj(ibA), sval = sval(ibA), 
                    score = score(ibA), truth = truth(ibA),
                    object_to_extend = ibB)
   expect_equal(pval(ibA), pval(ibC))
   expect_equal(padj(ibA), padj(ibC))
+  expect_equal(sval(ibA), sval(ibC))
   expect_equal(score(ibA), score(ibC))
   expect_equal(truth(ibA), truth(ibC))
 })
@@ -71,6 +74,8 @@ test_that("extending object with object without pval slot works", {
                                      row.names = paste0("F", 1:5)),
                    padj = data.frame(mA = c(0.1, 0.2, 0.3, 0.4, 0.5),
                                      row.names = paste0("F", 1:5)),
+                   sval = data.frame(mA = c(0.1, 0.2, 0.3, 0.4, 0.5),
+                                     row.names = paste0("F", 1:5)),
                    score = data.frame(mA = c(0.1, 0.2, 0.3, 0.4, 0.5),
                                       row.names = paste0("F", 1:5)),
                    truth = data.frame(status = c(1, 0, 1, 0, 1),
@@ -79,6 +84,7 @@ test_that("extending object with object without pval slot works", {
                    object_to_extend = ibA)
   expect_equal(pval(ibA), pval(ibB))
   expect_equal(padj(ibA), padj(ibB))
+  expect_equal(sval(ibA), sval(ibB))
   expect_equal(score(ibA), score(ibB))
   expect_equal(truth(ibA), truth(ibB))
 })
@@ -99,6 +105,8 @@ test_that("extending COBRAData objects works as expected", {
                                     row.names = paste0("F", 1:6)),
                   padj = data.frame(mA = c(0.6, 0.7, 0.8, 0.9, 1.0),
                                     row.names = paste0("F", 1:5)),
+                  sval = data.frame(mA = c(0.6, 0.7, 0.8, 0.9, 1.0),
+                                    row.names = paste0("F", 1:5)),
                   score = data.frame(mB = c(0.6, 0.7, 0.8),
                                      row.names = paste0("F", 1:3)),
                   truth = data.frame(status = c(1, 0, 1, 0, 1, 0),
@@ -109,21 +117,22 @@ test_that("extending COBRAData objects works as expected", {
   truth(ibBp)$status <- c(0, 1, 0, 1, 0, 1)
 
   ibtest <- COBRAData(pval = pval(ibA), padj = padj(ibA), score = score(ibA),
-                     truth = truth(ibA), object_to_extend = ibB)
+                      sval = sval(ibA), truth = truth(ibA), object_to_extend = ibB)
   expect_equal(nrow(pval(ibtest)), 6)
   expect_equal(pval(ibtest)[paste0("F", 1:6), "mB"], c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6))
   expect_equal(nrow(padj(ibtest)), 5)
   expect_equal(ncol(padj(ibtest)), 1)
   expect_equal(padj(ibtest)[paste0("F", 1:5), "mA"], c(0.6, 0.7, 0.8, 0.9, 1.0))
+  expect_equal(sval(ibtest)[paste0("F", 1:5), "mA"], c(0.6, 0.7, 0.8, 0.9, 1.0))
   expect_equal(score(ibtest)[paste0("F", 1:5), "mB"], c(0.6, 0.7, 0.8, NA, NA))
   expect_equal(truth(ibtest)[paste0("F", 1:6), "expr"], 1:6)
   expect_equal(ncol(truth(ibtest)), 2)
 
   expect_error(COBRAData(pval = pval(ibA), padj = padj(ibA), score = score(ibA),
-                        truth = truth(ibA), object_to_extend = ibBp))
+                         sval = sval(ibA), truth = truth(ibA), object_to_extend = ibBp))
 
   ibtestp <- COBRAData(pval = pval(ibBp), padj = padj(ibBp), score = score(ibBp),
-                      truth = truth(ibBp), object_to_extend = ibAp)
+                       sval = sval(ibBp), truth = truth(ibBp), object_to_extend = ibAp)
   expect_equal(pval(ibtest)[paste0("F", 1:6), c("mA", "mB")],
                pval(ibtestp)[paste0("F", 1:6), c("mA", "mB")])
   expect_equal(padj(ibtestp)[paste0("F", 1:5), "mA"], c(0.1, 0.2, 0.3, 0.4, 0.5))
@@ -164,6 +173,7 @@ test_that("replacement still returns valid objects", {
   cobradata <- cobradata_example_sval
   pval(cobradata) <- pval(cobradata)[1:500, ]
   padj(cobradata) <- padj(cobradata)[1:1000, ]
+  sval(cobradata) <- sval(cobradata)[1:1000, ]
   score(cobradata) <- score(cobradata)[1:750, ]
   truth(cobradata) <- truth(cobradata)[1:500, ]
   expect_is(cobradata, "COBRAData")
@@ -174,6 +184,8 @@ test_that("replacement still returns valid objects", {
   fdrtprcurve(cobraperf) <- fdrtprcurve(cobraperf)[1:2, ]
   fdrnbr(cobraperf) <- fdrnbr(cobraperf)[1:2, ]
   fdrnbrcurve(cobraperf) <- fdrnbrcurve(cobraperf)[1:2, ]
+  fsrnbr(cobraperf) <- fsrnbr(cobraperf)[1:2, ]
+  fsrnbrcurve(cobraperf) <- fsrnbrcurve(cobraperf)[1:2, ]
   fpr(cobraperf) <- fpr(cobraperf)[1:2, ]
   tpr(cobraperf) <- tpr(cobraperf)[1:2, ]
   roc(cobraperf) <- roc(cobraperf)[1:2, ]
@@ -196,6 +208,8 @@ test_that("replacement still returns valid objects", {
   fdrtprcurve(cobraplot) <- fdrtprcurve(cobraplot)[1:2, ]
   fdrnbr(cobraplot) <- fdrnbr(cobraplot)[1:2, ]
   fdrnbrcurve(cobraplot) <- fdrnbrcurve(cobraplot)[1:2, ]
+  fsrnbr(cobraplot) <- fsrnbr(cobraplot)[1:2, ]
+  fsrnbrcurve(cobraplot) <- fsrnbrcurve(cobraplot)[1:2, ]
   fpr(cobraplot) <- fpr(cobraplot)[1:2, ]
   tpr(cobraplot) <- tpr(cobraplot)[1:2, ]
   roc(cobraplot) <- roc(cobraplot)[1:2, ]
@@ -268,12 +282,39 @@ test_that("subsetting of objects works", {
 test_that("extending an object still returns valid objects", {
   cobradata <- cobradata_example_sval
   cobradata <- COBRAData(pval = pval(cobradata), object_to_extend = cobradata)
+  cobradata <- COBRAData(sval = sval(cobradata), object_to_extend = cobradata)
   expect_is(cobradata, "COBRAData")
 })
 
 test_that("extending an object of the wrong class doesn't work", {
-    cobradata <- cobradata_example_sval
-    cobraperf <- calculate_performance(cobradata, binary_truth = "status",
-                                      cont_truth = "logFC", aspects = "tpr")
-    expect_error(COBRAData(pval = pval(cobradata), object_to_extend = cobraperf))
+  cobradata <- cobradata_example_sval
+  cobraperf <- calculate_performance(cobradata, binary_truth = "status",
+                                     cont_truth = "logFC", aspects = "tpr")
+  expect_error(COBRAData(pval = pval(cobradata), object_to_extend = cobraperf))
+})
+
+test_that("updating COBRAData object works", {
+  expect_is(sval(cobradata_example), "data.frame")
+  cobradata <- update_cobradata(cobradata_example)
+  expect_is(cobradata, "COBRAData")
+  expect_is(cobradata@sval, "data.frame")
+})
+
+test_that("updating COBRAPerformance object works", {
+  cobradata <- cobradata_example_sval
+  cobraperf <- calculate_performance(cobradata, binary_truth = "status",
+                                     cont_truth = "logFC", aspects = "tpr")
+  expect_is(fsrnbr(cobraperf), "data.frame")
+  cobraperf <- update_cobraperformance(cobraperf)
+  expect_is(cobraperf, "COBRAPerformance")
+})
+
+test_that("reordering levels in COBRAPlot object works", {
+  cobraperf <- calculate_performance(calculate_adjp(cobradata_example_sval),
+                                     binary_truth = "status", aspects = "fpr")
+  cobraplot <- prepare_data_for_plot(cobraperf, colorscheme = "Dark2",
+                                     incltruth = TRUE)
+  cobraplot <- reorder_levels(cobraplot, c("Method3", "Method1"))
+  expect_is(cobraplot, "COBRAPlot")
+  expect_equal(levels(fpr(cobraplot)$method), c("Method3", "Method1", "Method2"))
 })
